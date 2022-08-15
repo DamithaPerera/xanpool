5cbbe86f311e6705c199f220f3f5402abfde105d
Damitha Perera <damitha.perera@keeneye.solutions>
2022-08-15 21:33:58 +0530

    e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +15,18 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('fixer API (e2e)', () => {
+    it('user should be able to get the currency rates', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/v1/fixer?base=USD&symbols=HKD')
+        .expect(200);
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body).toHaveProperty('success');
+      expect(response.body).toHaveProperty('data');
+      expect(response.body.data).toHaveProperty('base');
+      expect(response.body.data).toHaveProperty('date');
+      expect(response.body.data).toHaveProperty('rates');
+    });
   });
 });
